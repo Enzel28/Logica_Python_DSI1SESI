e8d3373ca0fc7babdbd66f781acac975b046a82e
Enzo <enzo.farias@edu.senai.br>
2026-05-20 16:04:28 -0300

    1.1-Exercícios-em-Python.py

--- 1.1-Exercício02-em-Python.py
+++ 1.1-Exercício02-em-Python.py
@@ -30,28 +30,60 @@ print("\n\n")
 # EX4
 # Tente converter a string "cento e vinte"
 # para inteiro e observe o que acontece.
+print("EX4")
+numero = "120"
+print(int(numero))
+print(type(numero))
+print("\n\n")
 
 # EX5
 # Converta o número 42 para string
 # e concatene com a palavra " respostas".
+print("EX5")
+numero = 42
+print(type(str(numero)))
+print("resposta" + "42")
+print("\n\n")
 
 # EX6
 # Use a função complex() para criar
 # um número complexo com parte real 3
 # e parte imaginária 5.
+print("EX6")
+real = 3
+imaginaria = 5
+print(complex(real + imaginaria))
+print("\n\n")
 
 # EX7
 # Converta o número 0 para booleano
 # e mostre o resultado.
+print("EX7")
+numero = 0
+print(bool(0))
+print("\n\n")
 
 # EX8
 # Converta o número -100 para booleano
 # e mostre o resultado.
+print("EX8")
+numero = -100
+print(bool(-100))
+print("\n\n")
 
 # EX9
 # Converta o número 3.1415 para inteiro
 # e depois para string, tudo em uma única linha.
+print("EX9")
+numero3 = str(int(3.1415))
+print("\n\n")
 
 # EX10
 # Some um número inteiro (5) com um float (2.3)
 # e verifique qual é o tipo do resultado.
+print("EX10")
+numero_int= 5
+numero_float= 2.3
+print("soma dos numeros:", numero_int + numero_float)
+print(type(numero))
+print("\n\n")
