1efb2aa20a41fe0a1fd7ceb8505f9c087c20bec5
Enzo <enzo.farias@edu.senai.br>
2026-05-20 10:41:10 -0300

    1.1-Exercício-em-Python.py

--- 1.1-Exercício01-em-Python.py
+++ 1.1-Exercício01-em-Python.py
@@ -1,46 +1,93 @@
 # EX1
 # Um aluno tem 10 anos. Armazene essa idade em uma variável
 # e exiba seu tipo.
+
+print("EX1")
 idade = 10
 print(type(idade))
 
+print("\n\n")
 
 # EX2
 # A temperatura medida é 23.5°C.
 # Armazene esse valor e mostre seu tipo.
+
+print("EX2")
 temp = int (23.5)
 print("int(23.5):", temp)
 print("tipo:", type(temp))
+print("\n\n")
 
 # EX3
 # Crie um número complexo representando uma impedância elétrica
 # de 5 + 8j e mostre sua parte real.
 
+print("EX3")
+numero_complexo = 5 + 8j
+print("valor:", numero_complexo)
+print("tipo:", type(numero_complexo))
+print("\n\n")
+
 # EX4
 # Mostre a parte imaginária do número complexo
 # criado no exercício anterior.
+
+print("EX4")
+numero_complexo = 23
+print ("parte imaginária", numero_complexo)
+print("\n\n")
 
 # EX5
 # Declare uma variável chamada "populacao"
 # com o valor 8_000_000_000 (8 bilhões)
 # e mostre seu tipo.
 
+print("EX5")
+população = 8_000_000_000
+print("População: 8.000.000.000", type(população))
+print("\n\n")
+
 # EX6
 # Verifique se o número 7 é do tipo int
 # usando a função type().
+
+print("EX6")
+numero = 7
+print("numero: 7", type(numero))
+print("\n\n")
 
 # EX7
 # Crie uma variável chamada "aprovado"
 # com o valor booleano True e mostre seu tipo.
 
+print("EX7")
+aprovado = True
+print("tipo:", type(aprovado))
+print("\n\n")
+
 # EX8
 # Some True e False e mostre o resultado
 # e também o tipo do resultado.
+
+print("EX8")
+soma = True + False
+print("valor:", soma)
+print("tipo:", type(soma))
+print("\n\n")
 
 # EX9
 # Pesquise e mostre qual é o valor máximo
 # que um número inteiro pode ter em Python.
 
+print("EX9")
+print("não existe um valor maximo para um numero inteiro no Python")
+print("\n\n")
+
 # EX10
 # Mostre a representação em binário
 # do número 10 usando uma função do Python.
+
+print("EX10")
+numero = 10
+print("representação em binario:", bin(10))
+print("\n\n")
