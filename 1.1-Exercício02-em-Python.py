e6f0a0be3ccb966fb12db6b96877971e9d66263f
Enzo <enzo.farias@edu.senai.br>
2026-05-20 13:59:38 -0300

    1.1-Exercícios02-em-Python.py

---
+++ 1.1-Exercício02-em-Python.py
@@ -0,0 +1,57 @@
+# EX1
+# O usuário digitou "25" como sua idade em um formulário.
+# Converta para inteiro e calcule a idade que ele terá
+# daqui a 5 anos.
+print("EX1")
+idade = 25
+print("idade:", idade + 5)
+print(type(idade))
+print("\n\n")
+
+# EX2
+# Converta o número de ponto flutuante 7.999
+# para inteiro e observe o resultado.
+print("EX2")
+numero = 7.999
+print(int(numero))
+print("numero(7.999)")
+print(type (numero))
+print("\n\n")
+
+# EX3
+# Converta a string "-3.14" para float
+# e multiplique o resultado por 2.
+print("EX3")
+string = -3.14
+print(float(string))
+print(string*2)
+print("\n\n")
+
+# EX4
+# Tente converter a string "cento e vinte"
+# para inteiro e observe o que acontece.
+
+# EX5
+# Converta o número 42 para string
+# e concatene com a palavra " respostas".
+
+# EX6
+# Use a função complex() para criar
+# um número complexo com parte real 3
+# e parte imaginária 5.
+
+# EX7
+# Converta o número 0 para booleano
+# e mostre o resultado.
+
+# EX8
+# Converta o número -100 para booleano
+# e mostre o resultado.
+
+# EX9
+# Converta o número 3.1415 para inteiro
+# e depois para string, tudo em uma única linha.
+
+# EX10
+# Some um número inteiro (5) com um float (2.3)
+# e verifique qual é o tipo do resultado.
